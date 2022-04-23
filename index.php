059aca1c5cf21c7349ad8433af2f6d20d759c3a4
jeancv24 <71114289+jeancv24@users.noreply.github.com>
2022-04-22 18:58:27 -0600

    test 1

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
 
+<h1>title</h1>
+
 <!-- content -->
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab molestiae repellendus illum consequatur atque cumque ea eos voluptates minus ipsum! Consequuntur quam esse fuga eius nostrum ullam in quaerat provident!</p>
 <!-- content -->  
