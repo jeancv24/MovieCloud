78b2495719ab842320b6aead0b0986e683ac6b2e
jeancv24 <71114289+jeancv24@users.noreply.github.com>
2022-04-25 17:56:39 -0600

    header construction

--- header.php
+++ header.php
@@ -6,7 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
-    <title>WORDPRESS</title>
+    <title>Movie Cloud</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet">
     
 </head>
 
@@ -16,17 +19,16 @@
         <!-- header -->
         <header>
             <!-- nav -->
-            <nav>
-                <ul>
-                    <li><a href="#">HOME</a></li>
-                    <li><a href="#">SERVICES</a></li>
-                    <li><a href="#">PORTFOLIO</a></li>
-                    <li><a href="#">CONTACT</a></li>
+            <nav class="">
+                <ul class="d-flex bd-highlight mb-3">
+                    <a class="me-auto p-2 bd-highlight" href=""><img class="main-logo" src="http://moviecloud.test/wp-content/uploads/2022/04/2-e1650926648292.png" alt="simple logo"></a>
+                    <li class="navBottons selected-nav bd-highlight"><a class="header-links" href="#">Home</a></li>
+                    <li class="navBottons bd-highlight"><a class="header-links" href="#">Top Best</a></li>
+                    <li class="navBottons bd-highlight"><a class="header-links" href="#">My List</a></li>
+                    <li class="navBottons bd-highlight"><a class="header-links" href="#">Trailer</a></li>
+                    <li class="navBottons bd-highlight"><a class="header-links" href="#">Login</a></li>
                 </ul>
             </nav>
             <!-- nav -->
-            <div>
-                <h1>MAIN TITLE - WORDPRESS</h1>
-            </div>
         </header>
         <!-- header -->
