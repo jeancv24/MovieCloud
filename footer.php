c07fed9224c4e9288e195994722bd93c25dd9836
aflores05 <aaron.flores@ucr.ac.cr>
2022-04-26 15:10:04 -0500

    Content y footer
    Se incluyó el content y el footer

--- footer.php
+++ footer.php
@@ -1,17 +1,12 @@
 <!-- footer -->
 <footer>
-            <div>
-                <nav>
-                    <ul>
-                        <li><a href="#">HOME</a></li>
-                        <li><a href="#">SERVICES</a></li>
-                        <li><a href="#">PORTFOLIO</a></li>
-                        <li><a href="#">CONTACT</a></li>
-                    </ul>
-                </nav>
-                <p>2022 &copy;</p>
-            </div>
-        </footer>
+    <nav class="">
+        <ul class="d-flex bd-highlight mb-3">
+            <a class="me-auto p-2 bd-highlight" href=""><img class="main-logo" src="http://moviecloud.test/wp-content/uploads/2022/04/2-e1650926648292.png" alt="simple logo"></a>
+                
+            </ul>
+    </nav>
+</footer>
         <!-- footer -->
 
     </div>
--- header.php
+++ header.php
@@ -9,13 +9,13 @@
     <title>Movie Cloud</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet">
-    
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
 </head>
 
 <body>
    <!-- container -->
-    <div>
+    <div class="">
         <!-- header -->
         <header>
             <!-- nav -->
