7ec19e36f0cd5c6a443c1958392225506941dd45
aflores05 <aaron.flores@ucr.ac.cr>
2022-04-26 12:01:23 -0500

    Prueba

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 
 <!-- content -->
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab molestiae repellendus illum consequatur atque cumque ea eos voluptates minus ipsum! Consequuntur quam esse fuga eius nostrum ullam in quaerat provident!</p>
+<img src="screenshot.png" alt="">
 <!-- content -->  
 
 <?php get_footer(); ?>
