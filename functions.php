326cd40d897d096db9358096bfb428174498b5d8
jeancv24 <71114289+jeancv24@users.noreply.github.com>
2022-05-23 20:40:50 -0600

    index responsive

--- functions.php
+++ functions.php
@@ -5,11 +5,16 @@
         //cargar BS
         wp_enqueue_style('bootstrap_css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
 
+        wp_enqueue_style('icons_css', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+
         //cargar BS JS
         wp_enqueue_script('bootstrap_js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js');
 
         //pertmite cargar el css del tema
         wp_enqueue_style('style', get_stylesheet_uri() );
+
+        //js
+        wp_enqueue_script( 'main_js', get_template_directory_uri() . '/main.js');
     }
 
     add_action('wp_enqueue_scripts', 'load_assets');
