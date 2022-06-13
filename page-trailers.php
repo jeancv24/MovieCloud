eff512341b590898bcec93462c7e96474726163a
jeancv24 <71114289+jeancv24@users.noreply.github.com>
2022-06-13 15:31:30 -0600

    translations

---
+++ page-trailers.php
@@ -0,0 +1,118 @@
+<?php get_header(); ?>
+
+<!-- title section -->
+<div class="top-container">
+    <img class="trailers-main-img" src="https://dev-peliresenas.pantheonsite.io/wp-content/uploads/2022/05/pexels-pixabay-33129-scaled-e1652134618688.jpg" alt="main img">
+    <h1 class="trailers-title">Trailers</h1>
+    <span class="line"></span>
+</div>
+<!-- title section -->
+
+<!-- content -->
+<div class="img-container">
+    <div class="row">
+        <div class="col-sm-2">                
+            <!-- Button trigger modal -->
+            <button type="button" class="trailer-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <img src="https://pics.filmaffinity.com/Nosotros-208891193-large.jpg" class="img" alt="">
+            </button>
+
+            <p class="movie-title">Lorem ipsum</p>
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-xl">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Trailer</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <iframe width="1280" height="720" src="https://www.youtube.com/embed/fWQrd6cwJ0A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-sm-2">
+            <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-630d9d1e6c7be72d25bec9a52accc428_screen.jpg?ts=1636999242" class="img" alt="">
+            <p class="movie-title">Lorem ipsum</p>
+        </div>
+        <div class="col-sm-2">
+            <img src="https://www.gamespot.com/a/uploads/original/1562/15626911/3776884-image%285%29.png" class="img" alt="">
+            <p class="movie-title">Lorem ipsum</p>
+        </div>
+        <div class="col-sm-2">
+            <img src="https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg" class="img" alt="">
+            <p class="movie-title">Lorem ipsum</p>
+        </div>
+        <div class="col-sm-2">
+            <img src="https://sm.ign.com/t/ign_za/gallery/s/spider-man/spider-man-far-from-home-official-movie-posters_ex7e.1080.jpg" class="img" alt="">
+            <p class="movie-title">Lorem ipsum</p>
+        </div>
+        <div class="col-sm-2">
+            <img src="https://i.pinimg.com/564x/b9/8e/e9/b98ee9a8e07f2e6a50e93644970f7b38.jpg" class="img" alt="">
+            <p class="movie-title">Lorem ipsum</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-2">                
+            <img src="https://pics.filmaffinity.com/Nosotros-208891193-large.jpg" class="img" alt="">
+            <p class="movie-title">Lorem ipsum</p>
+        </div>
+        <div class="col-sm-2">
+            <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-630d9d1e6c7be72d25bec9a52accc428_screen.jpg?ts=1636999242" class="img" alt="">
+            <p class="movie-title">Lorem ipsum</p>
+        </div>
+        <div class="col-sm-2">
+            <img src="https://www.gamespot.com/a/uploads/original/1562/15626911/3776884-image%285%29.png" class="img" alt="">
+            <p class="movie-title">Lorem ipsum</p>
+        </div>
+        <div class="col-sm-2">
+            <img src="https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg" class="img" alt="">
+            <p class="movie-title">Lorem ipsum</p>
+        </div>
+        <div class="col-sm-2">
+            <img src="https://sm.ign.com/t/ign_za/gallery/s/spider-man/spider-man-far-from-home-official-movie-posters_ex7e.1080.jpg" class="img" alt="">
+            <p class="movie-title">Lorem ipsum</p>
+        </div>
+        <div class="col-sm-2">
+            <img src="https://i.pinimg.com/564x/b9/8e/e9/b98ee9a8e07f2e6a50e93644970f7b38.jpg" class="img" alt="">
+            <p class="movie-title">Lorem ipsum</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-2">                
+            <img src="https://pics.filmaffinity.com/Nosotros-208891193-large.jpg" class="img" alt="">
+            <p class="movie-title">Lorem ipsum</p>
+        </div>
+        <div class="col-sm-2">
+            <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-630d9d1e6c7be72d25bec9a52accc428_screen.jpg?ts=1636999242" class="img" alt="">
+            <p class="movie-title">Lorem ipsum</p>
+        </div>
+        <div class="col-sm-2">
+            <img src="https://www.gamespot.com/a/uploads/original/1562/15626911/3776884-image%285%29.png" class="img" alt="">
+            <p class="movie-title">Lorem ipsum</p>
+        </div>
+        <div class="col-sm-2">
+            <img src="https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg" class="img" alt="">
+            <p class="movie-title">Lorem ipsum</p>
+        </div>
+        <div class="col-sm-2">
+            <img src="https://sm.ign.com/t/ign_za/gallery/s/spider-man/spider-man-far-from-home-official-movie-posters_ex7e.1080.jpg" class="img" alt="">
+            <p class="movie-title">Lorem ipsum</p>
+        </div>
+        <div class="col-sm-2">
+            <img src="https://i.pinimg.com/564x/b9/8e/e9/b98ee9a8e07f2e6a50e93644970f7b38.jpg" class="img" alt="">
+            <p class="movie-title">Lorem ipsum</p>
+        </div>
+    </div>
+</div>
+<!-- content -->  
+
+<?php get_footer(); ?>
