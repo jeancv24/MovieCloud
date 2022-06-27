2ff0328579995f70baf712d650d22490c758d5a9
jeancv24 <71114289+jeancv24@users.noreply.github.com>
2022-06-27 15:33:42 -0600

    trailer

--- page-home.php
+++ page-home.php
@@ -13,24 +13,30 @@
 <h1>MOVIES CATALOG</h1>
 
 <div class="img-container d-flex justify-content-center row">
-        <img src="https://pics.filmaffinity.com/Nosotros-208891193-large.jpg" class="col-sm-2 img" alt="">
-        <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-630d9d1e6c7be72d25bec9a52accc428_screen.jpg?ts=1636999242" class="col-sm-2 img" alt="">
-        <img src="https://www.gamespot.com/a/uploads/original/1562/15626911/3776884-image%285%29.png" class="col-sm-2 img" alt="">
-        <img src="https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg" class="col-sm-2 img" alt="">
-        <img src="https://sm.ign.com/t/ign_za/gallery/s/spider-man/spider-man-far-from-home-official-movie-posters_ex7e.1080.jpg" class="col-sm-2 img" alt="">
-        <img src="https://i.pinimg.com/564x/b9/8e/e9/b98ee9a8e07f2e6a50e93644970f7b38.jpg" class="col-sm-2 img" alt="">
-        <img src="https://pics.filmaffinity.com/Nosotros-208891193-large.jpg" class="col-sm-2 img" alt="">
-        <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-630d9d1e6c7be72d25bec9a52accc428_screen.jpg?ts=1636999242" class="col-sm-2 img" alt="">
-        <img src="https://www.gamespot.com/a/uploads/original/1562/15626911/3776884-image%285%29.png" class="col-sm-2 img" alt="">
-        <img src="https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg" class="col-sm-2 img" alt="">
-        <img src="https://sm.ign.com/t/ign_za/gallery/s/spider-man/spider-man-far-from-home-official-movie-posters_ex7e.1080.jpg" class="col-sm-2 img" alt="">
-        <img src="https://i.pinimg.com/564x/b9/8e/e9/b98ee9a8e07f2e6a50e93644970f7b38.jpg" class="col-sm-2 img" alt="">
-        <img src="https://pics.filmaffinity.com/Nosotros-208891193-large.jpg" class="col-sm-2 img" alt="">
-        <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-630d9d1e6c7be72d25bec9a52accc428_screen.jpg?ts=1636999242" class="col-sm-2 img" alt="">
-        <img src="https://www.gamespot.com/a/uploads/original/1562/15626911/3776884-image%285%29.png" class="col-sm-2 img" alt="">
-        <img src="https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg" class="col-sm-2 img" alt="">
-        <img src="https://sm.ign.com/t/ign_za/gallery/s/spider-man/spider-man-far-from-home-official-movie-posters_ex7e.1080.jpg" class="col-sm-2 img" alt="">
-        <img src="https://i.pinimg.com/564x/b9/8e/e9/b98ee9a8e07f2e6a50e93644970f7b38.jpg" class="col-sm-2 img" alt="">
+
+    <?php 
+        
+        $args = array(
+            'post_type'      => 'page',
+            'posts_per_page' => -1,
+            'post_parent'    => $post->ID,
+            'orderby'          => 'rand'
+        );
+        $parent = new WP_Query( $args );
+
+        if ( $parent->have_posts() ) : ?>
+
+            <?php while ( $parent->have_posts() ) : $parent->the_post(); ?>
+                
+                <tr class="movie-info" data-href="<?php the_permalink(); ?>">
+                    <td data-label="Photo"> <a class="col-sm-2 img-main-page-container" href="<?php the_permalink(); ?>"><img class="img" src="<?php the_field('imagen'); ?>"/></a></td>
+                </tr>
+            
+            <?php endwhile; ?>
+
+        <?php endif; wp_reset_postdata(); ?>
+
+    
 </div>
 <!-- content -->  
 
--- page-trailers.php
+++ page-trailers.php
@@ -9,109 +9,51 @@
 <!-- title section -->
 
 <!-- content -->
-<div class="img-container">
-    <div class="row">
-        <div class="col-sm-2">                
-            <!-- Button trigger modal -->
-            <button type="button" class="trailer-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                <img src="https://pics.filmaffinity.com/Nosotros-208891193-large.jpg" class="img" alt="">
-            </button>
+<div class="img-container d-flex justify-content-center row">
+    <?php 
+    
+    $args = array(
+        'post_type'      => 'page',
+        'posts_per_page' => -1,
+        'post_parent'    => $post->ID,
+        'orderby'          => 'rand'
+    );
+    $parent = new WP_Query( $args );
 
-            <p class="movie-title">Lorem ipsum</p>
-            <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-xl">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Trailer</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <iframe width="1280" height="720" src="https://www.youtube.com/embed/fWQrd6cwJ0A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+    if ( $parent->have_posts() ) : ?>
+
+        <?php while ( $parent->have_posts() ) : $parent->the_post(); ?>
+            
+            <tr class="movie-info" data-href="<?php the_permalink(); ?>">
+                <!-- imagen de link -->
+                <td data-label="Photo"> <button type="button" class="trailer-btn col-sm-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <img src="<?php the_field('imagen'); ?>" class="img" alt="">
+                </button></td>
+                <!-- modal -->
+                <td data-label="Name">
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-xl">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Trailer</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <iframe width="1280" height="720" src="<?php the_field('link'); ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                </td>
 
-        </div>
-        <div class="col-sm-2">
-            <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-630d9d1e6c7be72d25bec9a52accc428_screen.jpg?ts=1636999242" class="img" alt="">
-            <p class="movie-title">Lorem ipsum</p>
-        </div>
-        <div class="col-sm-2">
-            <img src="https://www.gamespot.com/a/uploads/original/1562/15626911/3776884-image%285%29.png" class="img" alt="">
-            <p class="movie-title">Lorem ipsum</p>
-        </div>
-        <div class="col-sm-2">
-            <img src="https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg" class="img" alt="">
-            <p class="movie-title">Lorem ipsum</p>
-        </div>
-        <div class="col-sm-2">
-            <img src="https://sm.ign.com/t/ign_za/gallery/s/spider-man/spider-man-far-from-home-official-movie-posters_ex7e.1080.jpg" class="img" alt="">
-            <p class="movie-title">Lorem ipsum</p>
-        </div>
-        <div class="col-sm-2">
-            <img src="https://i.pinimg.com/564x/b9/8e/e9/b98ee9a8e07f2e6a50e93644970f7b38.jpg" class="img" alt="">
-            <p class="movie-title">Lorem ipsum</p>
-        </div>
-    </div>
+            </tr>
+        
+        <?php endwhile; ?>
 
-    <div class="row">
-        <div class="col-sm-2">                
-            <img src="https://pics.filmaffinity.com/Nosotros-208891193-large.jpg" class="img" alt="">
-            <p class="movie-title">Lorem ipsum</p>
-        </div>
-        <div class="col-sm-2">
-            <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-630d9d1e6c7be72d25bec9a52accc428_screen.jpg?ts=1636999242" class="img" alt="">
-            <p class="movie-title">Lorem ipsum</p>
-        </div>
-        <div class="col-sm-2">
-            <img src="https://www.gamespot.com/a/uploads/original/1562/15626911/3776884-image%285%29.png" class="img" alt="">
-            <p class="movie-title">Lorem ipsum</p>
-        </div>
-        <div class="col-sm-2">
-            <img src="https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg" class="img" alt="">
-            <p class="movie-title">Lorem ipsum</p>
-        </div>
-        <div class="col-sm-2">
-            <img src="https://sm.ign.com/t/ign_za/gallery/s/spider-man/spider-man-far-from-home-official-movie-posters_ex7e.1080.jpg" class="img" alt="">
-            <p class="movie-title">Lorem ipsum</p>
-        </div>
-        <div class="col-sm-2">
-            <img src="https://i.pinimg.com/564x/b9/8e/e9/b98ee9a8e07f2e6a50e93644970f7b38.jpg" class="img" alt="">
-            <p class="movie-title">Lorem ipsum</p>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-sm-2">                
-            <img src="https://pics.filmaffinity.com/Nosotros-208891193-large.jpg" class="img" alt="">
-            <p class="movie-title">Lorem ipsum</p>
-        </div>
-        <div class="col-sm-2">
-            <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-630d9d1e6c7be72d25bec9a52accc428_screen.jpg?ts=1636999242" class="img" alt="">
-            <p class="movie-title">Lorem ipsum</p>
-        </div>
-        <div class="col-sm-2">
-            <img src="https://www.gamespot.com/a/uploads/original/1562/15626911/3776884-image%285%29.png" class="img" alt="">
-            <p class="movie-title">Lorem ipsum</p>
-        </div>
-        <div class="col-sm-2">
-            <img src="https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg" class="img" alt="">
-            <p class="movie-title">Lorem ipsum</p>
-        </div>
-        <div class="col-sm-2">
-            <img src="https://sm.ign.com/t/ign_za/gallery/s/spider-man/spider-man-far-from-home-official-movie-posters_ex7e.1080.jpg" class="img" alt="">
-            <p class="movie-title">Lorem ipsum</p>
-        </div>
-        <div class="col-sm-2">
-            <img src="https://i.pinimg.com/564x/b9/8e/e9/b98ee9a8e07f2e6a50e93644970f7b38.jpg" class="img" alt="">
-            <p class="movie-title">Lorem ipsum</p>
-        </div>
-    </div>
+    <?php endif; wp_reset_postdata(); ?>
 </div>
 <!-- content -->  
 
