d07338a1fe0edb4df1c9ce7d818af8a74aef54ef
jeancv24 <71114289+jeancv24@users.noreply.github.com>
2022-07-06 10:20:03 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <footer>
     <nav class="">
         <ul class="d-flex bd-highlight">
-            <a class="me-auto p-2 bd-highlight" href=""><img class="main-logo" src="https://dev-peliresenas.pantheonsite.io/wp-content/uploads/2022/04/2-e1650994672260.png" alt="simple logo"></a>
+            <a class="me-auto p-2 bd-highlight" href="<?php bloginfo('url'); ?>"><img class="main-logo" src="https://dev-peliresenas.pantheonsite.io/wp-content/uploads/2022/04/2-e1650994672260.png" alt="simple logo"></a>
         </ul>
     </nav>
 </footer>
