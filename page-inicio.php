188633805917cb83f5bd8ba242505e7e41977bc6
jeancv24 <71114289+jeancv24@users.noreply.github.com>
2022-07-12 14:50:58 -0600

    order inicio

--- page-inicio.php
+++ page-inicio.php
@@ -16,9 +16,13 @@
             'post_type'      => 'page',
             'posts_per_page' => -1,
             'post_parent'    => $post->ID,
-            'orderby'          => 'rand'
+            'orderby'          => array(
+                'meta_value_num',
+                'rate'
+            )
         );
         $parent = new WP_Query( $args );
+        print_r($parent);
 
         if ( $parent->have_posts() ) : ?>
 
